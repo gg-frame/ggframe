053086fb82dcc2c15167a04579eb3158737e5a1a
shuhei tanaka <shuhei46491@gmail.com>
2024-04-25 22:35:38 +0900

    fix fetch

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,58 +1,68 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
+import { fetchIPFSData, fetchPonder } from "@/hooks/useRegisteredEvent";
+import { Button, Frog, TextInput } from "frog";
+import { devtools } from "frog/dev";
 // import { neynar } from 'frog/hubs'
-import { handle } from 'frog/next'
-import { serveStatic } from 'frog/serve-static'
+import { handle } from "frog/next";
+import { serveStatic } from "frog/serve-static";
 
 const app = new Frog({
-  assetsPath: '/',
-  basePath: '/api',
+  assetsPath: "/",
+  basePath: "/api",
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-})
+});
 
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 
-app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+app.frame("/:chainId/:count", async (c) => {
+  const { buttonValue, inputText, status } = c;
+  const fruit = inputText || buttonValue;
+  const chainId = c.req.param("chainId");
+  const count = c.req.param("count");
+  const recipientCount = Number(count);
+
+  const data = await fetchPonder(count);
+  console.log(data);
+
+  // const metadata = await fetchIPFSData();
+
   return c.res({
     image: (
       <div
         style={{
-          alignItems: 'center',
+          alignItems: "center",
           background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
         }}
       >
         <div
           style={{
-            color: 'white',
+            color: "white",
             fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
             lineHeight: 1.4,
             marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          {status === "response"
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ""}`
+            : "Welcome!"}
         </div>
       </div>
     ),
@@ -61,12 +71,12 @@ app.frame('/', (c) => {
       <Button value="apples">Apples</Button>,
       <Button value="oranges">Oranges</Button>,
       <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      status === "response" && <Button.Reset>Reset</Button.Reset>,
     ],
-  })
-})
+  });
+});
 
-devtools(app, { serveStatic })
+devtools(app, { serveStatic });
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
