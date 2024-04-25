16d6441175245ad9682c645707edb750914a726c
shuhei tanaka <shuhei46491@gmail.com>
2024-04-25 22:35:09 +0900

    chore

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["ipfs.io"],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
