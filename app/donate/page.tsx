b9d4987c4bdb74daca281091d711121d010d04c7
shuhei tanaka <shuhei46491@gmail.com>
2024-05-30 21:58:47 +0900

    get recipientcount from param

---
+++ app/donate/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { getFrameMetadata } from "frog/next";
+import type { Metadata } from "next";
+import styles from "./page.module.css";
+
+interface Props {
+  params: { count: string };
+}
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const count = params.count;
+  const frameTags = await getFrameMetadata(
+    `https://gg-frame-three.vercel.app/api/donate/${count}`
+  );
+
+  return {
+    other: frameTags,
+  };
+}
+export default function Donate({ params }: Props) {
+  return <div>donate</div>;
+}
