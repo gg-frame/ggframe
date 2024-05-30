91a76ebfb4a670700aa16b357fec5084b904e5a1
shuhei tanaka <shuhei46491@gmail.com>
2024-05-30 21:05:28 +0900

    fix metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,33 @@
-import { getFrameMetadata } from 'frog/next'
-import type { Metadata } from 'next'
-import Image from 'next/image'
+import { getFrameMetadata } from "frog/next";
+import type { Metadata } from "next";
+import Image from "next/image";
 
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const frameTags = await getFrameMetadata(
-    `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
-  )
+  const frameTags = await getFrameMetadata(`${process.env.VERCEL_URL!}/api`);
   return {
     other: frameTags,
-  }
+  };
 }
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>app/page.tsx</code>
           </p>
           <p>
-            Head to{' '}
+            Head to{" "}
             <a
               href="/api/dev"
-              style={{ display: 'inline', fontWeight: 'semibold' }}
+              style={{ display: "inline", fontWeight: "semibold" }}
             >
               <code className={styles.code}>localhost:3000/api</code>
-            </a>{' '}
+            </a>{" "}
             for your frame endpoint.
           </p>
         </div>
@@ -39,7 +37,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -115,5 +113,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
