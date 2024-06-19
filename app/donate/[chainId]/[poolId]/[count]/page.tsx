9c8d3a4275a3391ed39378ddd54a1f479cb6b06e
shuhei tanaka <shuhei46491@gmail.com>
2024-06-19 23:31:13 +0900

    fix app/donate page

---
+++ app/donate/[chainId]/[poolId]/[count]/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { getFrameMetadata } from "frog/next";
+import type { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { poolId: string; chainId: string; count: string };
+}): Promise<Metadata> {
+  const frameTags = await getFrameMetadata(
+    `https://ggframe.xyz/api/donate/${params.chainId}/${params.poolId}/${params.count}`
+  );
+
+  return {
+    other: frameTags,
+  };
+}
+export default function Donate() {
+  return <div />;
+}
